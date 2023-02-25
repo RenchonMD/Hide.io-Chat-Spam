988420b3952996c91588d0a301303098519e749f
Renchon <92147511+RenchonMD@users.noreply.github.com>
2023-02-25 16:07:25 +0900

    Add files via upload

---
+++ Strings.h
@@ -0,0 +1,15 @@
+#include "Includes/Utils.h"
+typedef struct _monoString {
+    void *klass;
+    void *monitor;
+    int length;
+    char chars[1];
+
+    int getLength() {
+        return length;
+    }
+
+    char *getChars() {
+        return chars;
+    }
+} monoString;
